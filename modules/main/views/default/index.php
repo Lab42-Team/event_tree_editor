460e6ae893357ec573f8a49fb8b17e1df3972703
Nikita Dorodnykh <tualatin32@mail.ru>
2019-11-11 23:08:04 +0800

    Initial commit

---
+++ modules/main/views/default/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Extended Event Tree Editor';
+?>
+
+<div class="main-default-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to Extended Event Tree Editor!</h1>
+    </div>
+
+    <div class="body-content"></div>
+
+</div>
---
+++ modules/main/views/layouts/main.php
@@ -0,0 +1,85 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+use app\components\widgets\WLang;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="Content-Type" content="text/html">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+
+    <div class="wrap">
+        <?php
+        NavBar::begin([
+            'brandLabel' => Yii::$app->name,
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-inverse navbar-fixed-top',
+            ],
+        ]);
+        echo "<form class='navbar-form navbar-right'>" . WLang::widget() . "</form>";
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav navbar-right'],
+            'encodeLabels' => false,
+            'items' => array_filter([
+                ['label' => '<span class="glyphicon glyphicon-envelope"></span> ' .
+                    Yii::t('app', 'NAV_CONTACT_US'), 'url' => ['/main/default/contact']],
+                Yii::$app->user->isGuest ? (
+                ['label' => '<span class="glyphicon glyphicon-log-in"></span> ' . Yii::t('app', 'NAV_SIGN_IN'),
+                    'url' => ['/main/default/sing-in']]
+                ) : (
+                    '<li>'
+                    . Html::beginForm(['/main/default/sing-out'], 'post')
+                    . Html::submitButton(
+                        '<span class="glyphicon glyphicon-log-out"></span> ' . Yii::t('app', 'NAV_SIGN_OUT') .
+                        ' (' . Yii::$app->user->identity->username . ')',
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>'
+                )
+            ])
+        ]);
+        NavBar::end();
+        ?>
+
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <p class="pull-left"><?= ' &copy; ' . date('Y') . ' ' . Yii::t('app', 'FOOTER_INSTITUTE') ?></p>
+            <p class="pull-right"><?= Yii::t('app', 'FOOTER_POWERED_BY') . ' ' .
+                ' <a href="mailto:DorodnyxNikita@gmail.com">'.Yii::$app->params['adminEmail'].'</a>' ?></p>
+        </div>
+    </footer>
+
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
